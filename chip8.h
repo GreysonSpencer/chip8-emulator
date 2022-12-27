dd8a10df3c19cc97442a060f3a0170f591587700
Greyson Spencer <greyspencer1@gmail.com>
2022-12-27 12:51:43 -0600

    Add private on cpu registers and flags

--- chip8.h
+++ chip8.h
@@ -2,7 +2,29 @@
 #define CHIP8_H
 
 class chip8 {
+    private:
+        // Currently loaded opcode
+        unsigned short opcode;
 
+        // 4K of memory
+        //0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+        //0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
+        //0x200-0xFFF - Program ROM and work RAM
+        unsigned char memory[4096];
+
+        // 16 8-bit registers
+        unsigned char V[16];
+
+        // Index register
+        unsigned short I;
+
+        // Program counter
+        unsigned short pc;
+
+        // Delay and sound timer
+        // When above 0, will count down to 0 at 60hz
+        unsigned char delay_timer;
+        unsigned char sound_timer;
 };
 
 #endif
