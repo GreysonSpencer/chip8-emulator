dd56ba4c0a99745dadee80a606fbe052afa04c60
Greyson Spencer <greyspencer1@gmail.com>
2022-12-27 13:03:21 -0600

    Add initialize initial start

--- chip8.cpp
+++ chip8.cpp
@@ -2,7 +2,23 @@
 
 void chip8::initialize()
 {
-  // Initialize registers and memory once
+    pc = 0x200; // Every program starts at 0x200 or 512
+    opcode = 0; // Reset opcode
+    I = 0; // Reset index register
+    sp = 0; // Reset stack pointer
+
+    // Clear display
+    // Clear stack
+    // Clear registers V0-VF
+    // Clear memory
+
+    // Load fontset
+    // for (int i = 0; i < 80; i++)
+    // {
+    //     memory[i] = chip8_fontset[i];
+    // }
+    
+    // Reset timers
 }
  
 void chip8::emulateCycle()
