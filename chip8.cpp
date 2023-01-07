0d6c00efec30a0cb6e8cbe8974e38192b8d62676
Greyson Spencer <greyspencer1@gmail.com>
2023-01-07 11:29:17 -0600

    Add 8XY2

--- chip8.cpp
+++ chip8.cpp
@@ -147,6 +147,11 @@ void chip8::emulateCycle()
                 break;
                 // Sets Vx equal to Vx and Vy
                 case 0x0002:
+                    unsigned char x = opcode & 0x0F00;
+                    unsigned char y = opcode & 0x00F0;
+
+                    V[x] = V[x] & V[y];
+                    pc += 2;
                 break;
                 // Sets VX to VX xor VY
                 case 0x0003:
